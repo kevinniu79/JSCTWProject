faf5c14cfa36f952e8fa0b743d374711973e3a97
Holly R <hraetz14@gmail.com>
2022-06-15 13:46:26 -0500

    Box volume calculation
    Added a calculation of a rectangular box's volume

---
+++ driver.js
@@ -0,0 +1,5 @@
+let width = parseInt(prompt("Please input the width of the box: "));
+let height = parseInt(prompt("Please input the height: "));
+let length = parseInt(prompt("Please input the length: "));
+let volume = width*height*length;
+console.log("The volume of your box is "+volume+" units.");
