8359533c34c797e7f69e0421095048f26f2d43a0
Jason <jasondong03@gmail.com>
2022-06-15 13:45:35 -0500

    Volume of Box Program

---
+++ driver.js
@@ -0,0 +1,8 @@
+let length = parseFloat(prompt("Please enter the length of the box"));
+let width = parseFloat(prompt("Please enter the width of the box"));
+let height = parseFloat(prompt("Please enter the height of the box"));
+
+let volume = length*width*height;
+
+console.log("The volume of the box is " + volume);
+
