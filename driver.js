24b64b856502603874205e2b79abf5d874739c1e
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-06-15 13:50:28 -0500

    Volume_Aadi

---
+++ driver.js
@@ -0,0 +1,11 @@
+let side1;
+let side2;
+let side3;
+
+side1 = prompt("Side 1 length?");
+side2 = prompt("Side 2 width?");
+side3 = prompt("Side 3 height?");
+
+let volume;
+volume = side1 * side2 * side3;
+console.log("Volume: " + volume);
