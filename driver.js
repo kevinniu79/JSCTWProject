f50f341cf4beb2d2f7c6fc6403d206bf683709d0
Noah Ren <nren2025@gmail.com>
2022-06-15 13:45:30 -0500

    JS Practice
    JS Area

---
+++ driver.js
@@ -0,0 +1,8 @@
+let height = prompt("Please enter the height of the box");
+height = parseInt(height);
+let width = prompt("Please enter the width of the box");
+width = parseInt(width);
+let length = prompt("Please enter the length of the box");
+length = parseInt(length);
+let area = length * width * height;
+console.log(area);
