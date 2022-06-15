d942f57797b7cc6a2db9f10dab474e03b8c230ec
Aaditya Tiwari (BCHS) <24aaditf@gmail.com>
2022-06-15 14:15:10 -0500

    TestScores-Aadi

--- driver.js
+++ driver.js
@@ -1,4 +1,4 @@
-let side1;
+/*let side1;
 let side2;
 let side3;
 
@@ -9,3 +9,30 @@ side3 = prompt("Side 3 height?");
 let volume;
 volume = side1 * side2 * side3;
 console.log("Volume: " + volume);
+*/
+let score1;
+let score2;
+let score3;
+const denominator = 3;
+
+score1 = parseFloat(prompt("Score 1?"));
+score2 = parseFloat(prompt("Score 2?"));
+score3 = parseFloat(prompt("Score 3?"));
+
+let avg = (score1 + score2 + score3) / denominator;
+
+if(avg >= 92.5){
+    console.log("You get an A!");
+}
+if(avg >= 84.5 && avg < 92.5){
+    console.log("You get an B!");
+}
+if(avg >= 77.5 && avg < 84.5){
+    console.log("You get an C!");
+}
+if(avg >= 69.5 && avg < 77.5){
+    console.log("You get an D!");
+} 
+if(avg < 69.5){
+    console.log("You get an F!");
+}
