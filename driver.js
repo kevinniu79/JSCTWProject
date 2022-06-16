d3e347488c32bcd024e9c42267593c3345bff7ee
Sirat Mokha <mokha1671@gmail.com>
2022-06-16 10:19:07 -0500

    js

--- driver.js
+++ driver.js
@@ -8,15 +8,19 @@ for(let i = 0; i<numberOfPeople; i++){
 let name = prompt("What are the names of those people? ");
 arr[i] = name; 
 }
-
-for(let i = 0; i<arr.length; i++){
-    let blank = arr[i].indexOf(" ");
-    let lastName = arr[i].substring(blank, arr[i].length);  
-   console.log( Math.max(lastName.length - 1)); 
+function getLastName(name){
+    let blank = name.indexOf(" ");
+    let lastName = name.substring(blank, name.length);  
 }
-for(let i = 0; i<arr.length; i++){
-    let blank = arr[i].indexOf(" ");
-    let lastName = arr[i].substring(blank, arr[i].length);  
-    console.log(lastName); 
+
+const newArr = arr.map(getLastName); 
+
+for(let i = 0; i<newArr.length; i++){
+   
+   console.log( Math.max(newArr[i].length - 1)); 
+}
+
+for(let i = 0; i<newArr.length; i++){
+    console.log(newArr[i]); 
 }
 
