2842f0fdfc6e774d7003af0b603369d9dec4422b
Holly R <hraetz14@gmail.com>
2022-06-16 10:16:48 -0500

    Name length function part 2

--- driver.js
+++ driver.js
@@ -1,25 +1,35 @@
 let peopleNum = parseInt(prompt("How many people do you know?"));
 const peopleList = [];
-let nameLength = 0;
-let shortestNameLength = 100;
-let names = [];
-longestNameIndex = 0;
 for(let i = 0; i<peopleNum; i++){
     peopleList[i] = prompt("Please input the name of a person you know:");
 }
-for(let i = 0; i<peopleList.length; i++){
-    names = peopleList[i].split(" ");
-    if(names[1].length > nameLength){
-        nameLength = names[1].length;
-        longestNameIndex = i;
-    }
-    if(names[1].length < shortestNameLength){
-        shortestNameLength = names[1].length;
-    }
-}
-console.log("The person you know with the longest last name is: "+peopleList[longestNameIndex]);
-console.log("The length of last names of people you submitted was from "+shortestNameLength+" to "+nameLength);
+let nameInfo = namesInfo(peopleList);
+console.log("The person you know with the longest last name is: "+nameInfo.longestName);
+console.log("The length of last names of people you submitted was from "+nameInfo.shortestLength+" to "+nameInfo.longestLength);
 console.log("The people you submitted were: ");
 for(let i = 0; i<peopleList.length; i++){
     console.log(peopleList[i]);
+}
+
+function namesInfo(listOfNames){
+    const info = {
+        longestName : "",
+        shortestLength : 0,
+        longestLength : 0
+    }
+    let names = listOfNames[0].split(" ");
+    info.shortestLength = names[1].length;
+    let longestNameIndex = 0;
+    for(let i = 0; i<listOfNames.length; i++){
+        names = listOfNames[i].split(" ");
+        if(names[1].length > info.longestLength){
+            info.longestLength = names[1].length;
+            longestNameIndex = i;
+        }
+        if(names[1].length < info.shortestLength){
+            info.shortestLength = names[1].length;
+        }
+    }
+    info.longestName = listOfNames[longestNameIndex];
+    return info;
 }
